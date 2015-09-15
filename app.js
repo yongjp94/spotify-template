610b6b9db43c12d730df4ca67dea68a65989d77e
Michael Freeman <michael.k.freeman@gmail.com>
2015-09-15 12:30:05 -0700

    Init commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var data;
+var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
+var myApp = angular.module('myApp', [])
+
+var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
+  $scope.audioObject = {}
+  $scope.getSongs = function() {
+    $http.get(baseUrl + $scope.track).success(function(response){
+      data = $scope.tracks = response.tracks.items
+      
+    })
+  }
+  $scope.play = function(song) {
+    if($scope.currentSong == song) {
+      $scope.audioObject.pause()
+      $scope.currentSong = false
+      return
+    }
+    else {
+      if($scope.audioObject.pause != undefined) $scope.audioObject.pause()
+      $scope.audioObject = new Audio(song);
+      $scope.audioObject.play()  
+      $scope.currentSong = song
+    }
+  }
+})
+
+// Add tool tips to anything with a title property
+$('body').tooltip({
+    selector: '[title]'
+});
